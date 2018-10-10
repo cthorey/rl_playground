8dc390ac386e17b41d06097f65cde121b3e9b0bf
clement thorey <clement.thorey@gmail.com>
2018-10-10 19:28:15 +0100

    add docker

---
+++ docker/shortcut.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rl() {
+    docker run  \
+           -e ROOT_DIR='/workdir' \
+           -v $HOME/workdir/reinforcement-learning:/workdir \
+           -p "8888:8888" \
+           -p "5901:5901" \
+           -p "6901:6901" \
+           --rm \
+           -it \
+           rl /run_jupyter.sh
+}
+
